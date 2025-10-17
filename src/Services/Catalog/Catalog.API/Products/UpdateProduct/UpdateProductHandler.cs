f4f3551cbb416fd468de81a9644b13740bf977c4
onura <onurabdulaji@gmail.com>
2025-10-17 21:55:56 +0200

    Develop CRUD Command Validators with Fluent Validation Pipeline Behaviour

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -6,6 +6,21 @@ namespace Catalog.API.Products.UpdateProduct
 
     public record UpdateProductResult(bool IsSuccess);
 
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product Id is required.");
+
+            RuleFor(command => command.Name)
+                .NotEmpty().WithMessage("Product name is required.")
+                .Length(2,100).WithMessage("Product name muste be beetwen 2-100 character.");
+
+            RuleFor(command => command.Price)
+                .GreaterThan(0).WithMessage("Product price must be greater than zero.");
+        }
+    }
+
     internal class UpdateProductCommandHandler 
         (IDocumentSession session , ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
