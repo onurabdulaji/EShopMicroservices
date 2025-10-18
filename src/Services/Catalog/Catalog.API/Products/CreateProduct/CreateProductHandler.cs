54d1e3c5e54ecbac8508b403ee52625ed902c74e
onura <onurabdulaji@gmail.com>
2025-10-18 23:39:43 +0200

    Test Logging Behaviour for all MediatR Requests on Catalog.API microservices

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -13,14 +13,11 @@
         }
     }
     internal class CreateProductCommandHandler
-        (IDocumentSession session , ILogger<CreateProductCommandHandler> logger) 
+        (IDocumentSession session) 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-
-            logger.LogInformation("CreateProductCommandHandler.Handle called with command: {@command}", command);
-
             var product = new Product
             {
                 Name = command.Name,
