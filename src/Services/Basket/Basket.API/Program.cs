34a5eca4d3d6953c82a68acc33f6f99169b3bece
onura <onurabdulaji@gmail.com>
2025-10-19 17:59:07 +0200

    126. Exception Handling in Basket Microservices

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -21,10 +21,14 @@ builder.Services.AddMarten(options =>
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
