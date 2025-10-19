9234e080f05938a6cb8a1e4dd95b85fca17380e1
onura <onurabdulaji@gmail.com>
2025-10-19 17:15:25 +0200

    114. Develop Delete Basket Endpoint with Minimal Apis and Carter

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,8 +1,24 @@
 ﻿namespace Basket.API.Basket.DeleteBasket
 {
-    public record DeleteBasketRequest();
+    //public record DeleteBasketRequest(string UserName);
     public record DeleteBasketResponse(bool IsSuccess);
-    public class DeleteBasketEndpoint
+    public class DeleteBasketEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{username}" , async (string userName , ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(userName));
+
+                var response = result.Adapt<DeleteBasketCommandResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("DeleteBasket")
+                .Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Delete basket by username")
+                .WithDescription("Deletes a basket by its username.");
+        }
     }
 }
