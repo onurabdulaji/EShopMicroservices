f26e33369c8e66ae84f890b9956220c231d9b314
onura <onurabdulaji@gmail.com>
2025-10-19 20:38:41 +0200

    132. Register Decorated CachedBasketRepository and StackExchangeRedis with Scrutor

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -20,6 +20,14 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis")!;
+    // options.InstanceName = "Basket";
+});
+
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
@@ -32,3 +40,44 @@ app.MapCarter();
 app.UseExceptionHandler(options => { });
 
 app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//builder.Services.AddScoped<IBasketRepository>(provider =>
+//{
+//    var basketRepository = provider.GetRequiredService<BasketRepository>();
+//    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+//});
