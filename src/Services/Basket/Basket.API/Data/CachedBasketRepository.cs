160941333bca2835fe8471ee79284fa3ef867ad5
onura <onurabdulaji@gmail.com>
2025-10-19 18:18:12 +0200

    131. Develop CachedBasketRepository with Proxy Pattern, Decorator Pattern and Scrutor

--- src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -1,22 +1,40 @@
-﻿
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
 namespace Basket.API.Data
 {
     public class CachedBasketRepository
-        (IBasketRepository repository)
+        (IBasketRepository repository , IDistributedCache cache)
         : IBasketRepository
     {
         public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
         {
-            return await repository.GetBasket(userName, cancellationToken);
+            var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+            if (!string.IsNullOrEmpty(cachedBasket))
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
+            var basket = await repository.GetBasket(userName, cancellationToken);
+
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
         }
 
         public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
         {
-            return await repository.StoreBasket(basket, cancellationToken);
+            await repository.StoreBasket(basket, cancellationToken);
+
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
         }
         public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
         {
-            return await repository.DeleteBasket(userName, cancellationToken);
+            await repository.DeleteBasket(userName, cancellationToken);
+
+            await cache.RemoveAsync(userName, cancellationToken);
+
+            return true;
         }
     }
 }
