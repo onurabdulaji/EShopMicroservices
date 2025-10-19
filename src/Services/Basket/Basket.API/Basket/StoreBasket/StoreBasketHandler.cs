50ed1f0f86face601a78cefe40cc00b620da647a
onura <onurabdulaji@gmail.com>
2025-10-19 14:48:12 +0200

    111. Develop StoreBasket Feature Handler class with CQRS and MediatR

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,22 @@
 ﻿namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(q=>q.Cart).NotNull().WithMessage("Cart cannot be null");
+            RuleFor(q=>q.Cart.UserName).NotEmpty().WithMessage("UserName cannot be empty");
+        }
+    }
+    public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            return new StoreBasketResult("swn");
+        }
     }
 }
