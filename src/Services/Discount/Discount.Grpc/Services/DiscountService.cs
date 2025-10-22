25ab03224b3dad19cd201eeeb13df54db7e01b74
onura <onurabdulaji@gmail.com>
2025-10-22 23:50:42 +0200

    Develop DiscountService class to Implement Grpc Proto Service Methods

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,12 +1,29 @@
-﻿using Grpc.Core;
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.Grpc.Services
 {
-    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    public class DiscountService
+        (DiscountContext dbContext,ILogger<DiscountService> logger)
+        : DiscountProtoService.DiscountProtoServiceBase
     {
-        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
         {
-            return base.GetDiscount(request, context);
+            var coupon = await dbContext.Coupons
+                .FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+
+            if (coupon is null)
+                coupon = new Coupon { ProductName="No Discount",Description = "No Discount Description",Amount = 0 };
+
+            logger.LogInformation("Discount retrieved for ProductName : {ProductName}, Amount : {Amount}",
+                coupon.ProductName, coupon.Amount);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+
+            return couponModel;
         }
 
         public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
