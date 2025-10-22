27762d3d80696ea16f3ebeb57e9d4b391264576a
onura <onurabdulaji@gmail.com>
2025-10-22 23:58:23 +0200

    Develop UpdateDiscount in DiscountService class to Implement CRUD Grpc Proto

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -44,9 +44,22 @@ namespace Discount.Grpc.Services
             return couponModel;
         }
 
-        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
         {
-            return base.UpdateDiscount(request, context);
+            var coupon = request.Coupon.Adapt<Coupon>();
+
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid Request Object !"));
+
+            dbContext.Coupons.Update(coupon);
+
+            await dbContext.SaveChangesAsync();
+
+            logger.LogInformation("Discount successfully Updated. ProductName : {ProductName}", coupon.ProductName);
+
+            var couponModel = coupon.Adapt<CouponModel>();
+
+            return couponModel;
         }
 
         public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
