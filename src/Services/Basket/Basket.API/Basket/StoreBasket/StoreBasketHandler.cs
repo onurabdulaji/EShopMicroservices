392deac09f9a2fa3d271026a02d64321c1c806a8
onura <onurabdulaji@gmail.com>
2025-10-23 02:07:09 +0200

    Consuming Discount Grpc Service From Basket API Microservice When Adding Cart -2

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,7 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿using Discount.Grpc;
+using JasperFx.Events.Daemon;
+
+namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string UserName);
@@ -11,18 +14,25 @@
         }
     }
     public class StoreBasketCommandHandler
-        (IBasketRepository repository)
+        (IBasketRepository repository , DiscountProtoService.DiscountProtoServiceClient discountProto)
         : ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
-            // Communicate With gRPC For Discount Service To Retrieve Discounts For Each Item In The Cart
-
-            ShoppingCart cart = command.Cart;
+            await DeductDiscount(command.Cart , cancellationToken);
 
             await repository.StoreBasket(command.Cart , cancellationToken);
 
             return new StoreBasketResult(command.Cart.UserName);
+        }
+
+        private async Task DeductDiscount(ShoppingCart cart , CancellationToken cancellationToken)
+        {
+            foreach (var item in cart.Items)
+            {
+                var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+                item.Price -= coupon.Amount;
+            }
         }
     }
 }
