b2a63b11583d73948de488fe6f7ba7ab1bc70d4e
onura <onurabdulaji@gmail.com>
2025-10-24 00:23:55 +0200

    Develop Domain Modeling with DDD for Ordering Microservices

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record Address
+    {
+        public string FirstName { get;} = default!;
+        public string LastName { get;} = default!;
+        public string? EmailAdress { get;} = default!;
+        public string AdressLine { get;} = default!;
+        public string Country { get;} = default!;
+        public string State { get;} = default!;
+        public string ZipCode { get;} = default!;
+    }
+}
