94fae1be00a2d851f76b1b2925743c9596e7847b
onura <onurabdulaji@gmail.com>
2025-10-24 14:49:20 +0200

    Develop Dto Classes for Orders in Ordering.Application Layer

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Dtos
+{
+    public record AddressDto
+        (
+         string FirstName,
+         string LastName,
+         string EmailAdress,
+         string AddressLine,
+         string Country,
+         string State,
+         string ZipCode
+        );
+}
