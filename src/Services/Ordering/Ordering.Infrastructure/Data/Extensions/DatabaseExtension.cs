c551925b23bfd5f04e2880e286dc37fc3d0330f5
onura <onurabdulaji@gmail.com>
2025-10-24 11:34:42 +0200

    Seed SQL Server OrderDb using EF Core when Application Startup with Coding

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtension.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtension.cs
@@ -12,7 +12,21 @@ namespace Ordering.Infrastructure.Data.Extensions
 
             context.Database.MigrateAsync().GetAwaiter().GetResult();
 
+            await SeedAsync(context);
+        }
 
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            await SeedCustomersAsync(context);
+        }
+
+        private static async Task SeedCustomersAsync(ApplicationDbContext context)
+        {
+            if(!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
