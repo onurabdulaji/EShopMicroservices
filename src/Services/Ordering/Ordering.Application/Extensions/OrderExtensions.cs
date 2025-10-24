025b2a98920f4160dd4996219be1a7aeeb8f0bb0
onura <onurabdulaji@gmail.com>
2025-10-24 21:30:00 +0200

    Develop Extention method for ProjectToOrderDto use in GetOrders Methods

---
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Ordering.Application.Extensions
+{
+    public static class OrderExtensions
+    {
+        public static IEnumerable<OrderDto> ToOrderDtoList(this IEnumerable<Order> orders)
+        {
+            return orders.Select(order => new OrderDto(
+
+                Id: order.Id.Value,
+
+                CustomerId: order.CustomerId.Value,
+
+                OrderName: order.OrderName.Value,
+
+                ShippingAddress: new AddressDto(order.ShippingAddress.FirstName, order.ShippingAddress.LastName, order.ShippingAddress.EmailAdress!, order.ShippingAddress.AdressLine, order.ShippingAddress.Country, order.ShippingAddress.State, order.ShippingAddress.ZipCode),
+
+                BillingAddress: new AddressDto(order.BillingAddress.FirstName, order.BillingAddress.LastName, order.BillingAddress.EmailAdress!, order.BillingAddress.AdressLine, order.BillingAddress.Country, order.BillingAddress.State, order.BillingAddress.ZipCode),
+
+                Payment: new PaymentDto(order.Payment.CardName!, order.Payment.CardNumber, order.Payment.Expiration, order.Payment.CVV, order.Payment.PaymentMethod),
+
+                Status: order.Status,
+
+                OrderItems: order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
+
+                ));
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName
 {
     public class GetOrdersByNameHandler
         (IApplicationDbContext dbContext)
@@ -13,52 +15,7 @@
                 .OrderBy(q => q.OrderName)
                 .ToListAsync(cancellationToken);
 
-            var orderDtos = ProjectToOrderDto(orders);
-
-            return new GetOrdersByNameResult(orderDtos);
-        }
-
-        private List<OrderDto> ProjectToOrderDto(List<Order> orders)
-        {
-            List<OrderDto> result = new();
-
-            foreach (var order in orders)
-            {
-                var orderDto = new OrderDto(
-                        Id: order.Id.Value,
-                        CustomerId: order.CustomerId.Value,
-                        OrderName: order.OrderName.Value,
-                        ShippingAddress: new AddressDto(
-                            order.ShippingAddress.FirstName,
-                            order.ShippingAddress.LastName,
-                            order.ShippingAddress.EmailAdress,
-                            order.ShippingAddress.AdressLine,
-                            order.ShippingAddress.Country,
-                            order.ShippingAddress.State,
-                            order.ShippingAddress.ZipCode
-                            ),
-                        BillingAddress: new AddressDto(
-                            order.BillingAddress.FirstName,
-                            order.BillingAddress.LastName,
-                            order.BillingAddress.EmailAdress,
-                            order.BillingAddress.AdressLine,
-                            order.BillingAddress.Country,
-                            order.BillingAddress.State,
-                            order.BillingAddress.ZipCode
-                            ),
-                        Payment: new PaymentDto(
-                            order.Payment.CardName,
-                            order.Payment.CardNumber,
-                            order.Payment.Expiration,
-                            order.Payment.CVV,
-                            order.Payment.PaymentMethod),
-                        Status: order.Status,
-                        OrderItems: order.OrderItems.Select
-                        (oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
-                    );
-                result.Add(orderDto);
-            }
-            return result;
+            return new GetOrdersByNameResult(orders.ToOrderDtoList());
         }
     }
 }
