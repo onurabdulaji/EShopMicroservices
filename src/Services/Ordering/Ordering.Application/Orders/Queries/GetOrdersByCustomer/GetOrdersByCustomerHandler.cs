623970ea92655ae83fc92ad6953fa6264774dc7a
onura <onurabdulaji@gmail.com>
2025-10-24 21:42:11 +0200

    Develop GetOrdersByCustomer Queries w/ CQRS and MediatR in Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,22 @@
+﻿
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrdersByCustomerHandler
+        (IApplicationDbContext dbContext)
+        : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(q =>q.OrderItems)
+                .AsNoTracking()
+                .Where(q => q.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(q => q.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
