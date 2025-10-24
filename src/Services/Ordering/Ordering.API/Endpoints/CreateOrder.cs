d1f960429f1387159c823795b443d5650de659bd
onura <onurabdulaji@gmail.com>
2025-10-24 22:22:32 +0200

    Developing Order Endpoints for REST APIs in Ordering.API Layer

---
+++ src/Services/Ordering/Ordering.API/Endpoints/CreateOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.API.Endpoints
+{
+    // Accept a CreateOrderRequest object.
+    // Maps The request to a CreateOrderCommand.
+    // Uses MediatR to send the command to the corresponding handler.
+    // Return a response with created order's ID.
+    public class CreateOrder
+    {
+    }
+}
