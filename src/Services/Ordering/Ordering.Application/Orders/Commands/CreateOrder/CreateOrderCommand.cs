deabd84acab7965eef456d535f608f75ed85a3e9
onura <onurabdulaji@gmail.com>
2025-10-24 14:59:59 +0200

    Develop Orders CreateOrderCommand and Handler in Ordering.Application Layer

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+    public record CreateOrderResult(Guid Id);
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(q => q.Order.OrderName).NotEmpty().WithMessage("Name is Requried!"); 
+            RuleFor(q => q.Order.CustomerId).NotNull().WithMessage("Customer ID is Requried!"); 
+            RuleFor(q => q.Order.OrderItems).NotEmpty().WithMessage("OrderItems should not be Empty!"); 
+        }
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+        {
+            // Create Order Entity from Command Object
+            // Save To Db
+            // Return Result
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
