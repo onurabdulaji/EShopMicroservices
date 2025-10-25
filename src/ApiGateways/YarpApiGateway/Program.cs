16e5bb479ffe65be6e84e7353f839a2797982e7f
onura <onurabdulaji@gmail.com>
2025-10-25 20:54:07 +0200

    Rate Limiter ,

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services To Container
@@ -5,9 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP Request Pipeline
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
