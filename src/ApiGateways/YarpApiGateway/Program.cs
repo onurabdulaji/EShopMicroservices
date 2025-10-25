2e9c593dda95e06eb1b21fbba3dfa475f682f823
onura <onurabdulaji@gmail.com>
2025-10-25 20:42:19 +0200

    Develop Yarp Reverse Proxy in YarpApiGateway Microservices

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -2,8 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add Services To Container
 
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
 // Configure the HTTP Request Pipeline
+
+app.MapReverseProxy();
 
 app.Run();
