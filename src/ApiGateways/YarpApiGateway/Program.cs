1b80953587a3e5680a132324e5e4473ef6ae95ec
onura <onurabdulaji@gmail.com>
2025-10-25 22:03:14 +0200

    Docker YARP Fix

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,17 +1,42 @@
 using Microsoft.AspNetCore.RateLimiting;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.Listen(System.Net.IPAddress.Any, 8080, listenOptions =>
-    {
-        //listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
-    });
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.Listen(System.Net.IPAddress.Any, 8080, listenOptions =>
+//    {
+//        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+//    });
 
-    //options.Listen(System.Net.IPAddress.Any, 8081);
-});
+//    options.Listen(System.Net.IPAddress.Any, 8081);
+//});
+
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.Listen(System.Net.IPAddress.Any, 8081, listenOptions =>
+//    {
+//        listenOptions.UseHttps(httpsOptions =>
+//        {
+//            try
+//            {
+//                httpsOptions.ServerCertificate = new X509Certificate2(
+//                    "/home/app/.aspnet/https/aspnetapp.pfx"
+//                );
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine($"WARNING: HTTPS certificate failed to load. Falling back to HTTP. Error: {ex.Message}");
+//            }
+//        });
+
+//        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
+//    });
+
+//    options.Listen(System.Net.IPAddress.Any, 8080);
+//});
 
 // Add Services To Container
 
