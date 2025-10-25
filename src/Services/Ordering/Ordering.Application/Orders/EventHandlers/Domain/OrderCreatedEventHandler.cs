30a1c487da0280d9228fdd7210efee48910027c5
onura <onurabdulaji@gmail.com>
2025-10-25 16:37:03 +0200

    Publish OrderCreated Integration event for Order Fullfiment microservices

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,13 +1,19 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain
+﻿using MassTransit;
+using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
 {
     public class OrderCreatedEventHandler
-        (ILogger<OrderCreatedEventHandler> logger)
+        (ILogger<OrderCreatedEventHandler> logger , IPublishEndpoint publishEndpoint)
         : INotificationHandler<OrderCreatedEvent>
     {
-        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Domain Event Handled : {DomainEvent}", notification.GetType().Name);
-            return Task.CompletedTask;
+            logger.LogInformation("Domain Event Handled : {DomainEvent}", domainEvent.GetType().Name);
+
+            var orderCratedIntegrationEvet = domainEvent.Order.ToOrderDto();
+
+            await publishEndpoint.Publish(orderCratedIntegrationEvet, cancellationToken);
         }
     }
 }
